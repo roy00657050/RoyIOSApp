9885d27772195fd0eb1ec62451b8a2ce9e34b775
roy <00657050sityukhang@gmail.com>
2020-06-15 03:39:03 -0700

    Initial Commit

---
+++ MyAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAPP
+//
+//  Created by Roy on 2020/6/15.
+//  Copyright © 2020 Roy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
